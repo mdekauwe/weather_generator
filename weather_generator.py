f16eaab94a8d7bacbb8c1fa1554b5e84097e2f00
mdekauwe <mdekauwe@gmail.com>
2016-03-21 14:08:51 +1100

    Added solar geometry functions

--- weather_generator.py
+++ weather_generator.py
@@ -7,7 +7,7 @@ AWAP weather generator functions
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from math import pi, cos, sin, exp, sqrt
+from math import pi, cos, sin, exp, sqrt, acos
 import random
 
 __author__  = "Martin De Kauwe"
@@ -21,11 +21,19 @@ def main():
     tmax = 24.0
     doy = 180.0
     sw_rad_day = 50.0
-    lat = 50.0
+    lat = 51.5
+    lon = -0.13
     hours = np.arange(48) / 2.0
 
     (par, day_length) = estimate_dirunal_par(lat, doy, sw_rad_day)
 
+    (elevation, cos_zenith) = calculate_solar_geometry(doy, lat, lon)
+
+
+    plt.plot(hours, elevation, "r-")
+    plt.show()
+
+    sys.exit()
 
     #plt.plot(hours, par, "r-")
     #plt.ylabel("par ($\mu$mol m$^{-2}$ s$^{-1}$)")
@@ -300,6 +308,220 @@ def estimate_diurnal_temp(tmin, tmax, day_length):
 
 
     return (tday)
+
+def calculate_solar_geometry(doy, latitude, longitude):
+
+    """
+    The solar zenith angle is the angle between the zenith and the centre
+    of the sun's disc. The solar elevation angle is the altitude of the
+    sun, the angle between the horizon and the centre of the sun's disc.
+    Since these two angles are complementary, the cosine of either one of
+    them equals the sine of the other, i.e. cos theta = sin beta. I will
+    use cos_zen throughout code for simplicity.
+    Arguments:
+    ----------
+    params : p
+        params structure
+    doy : double
+        day of year
+    hod : double:
+        hour of the day [0.5 to 24]
+    cos_zen : double
+        cosine of the zenith angle of the sun in degrees (returned)
+    elevation : double
+        solar elevation (degrees) (returned)
+    References:
+    -----------
+    * De Pury & Farquhar (1997) PCE, 20, 537-557.
+    """
+
+    elevation = np.zeros(48)
+    cos_zenith = np.zeros(48)
+    for i in xrange(1, 48+1):
+        # need to convert 30 min data, 0-47 to 0-23.5
+        hod = i / 2.0
+
+        gamma = day_angle(doy)
+        dec = calculate_solar_declination(doy, gamma)
+        et = calculate_eqn_of_time(gamma)
+        t0 = calculate_solar_noon(et, longitude)
+        h = calculate_hour_angle(hod, t0)
+        rlat = latitude * pi / 180.0
+
+        # A13 - De Pury & Farquhar
+        sin_beta = sin(rlat) * sin(dec) + cos(rlat) * cos(dec) * cos(h)
+        cos_zenith[i-1] = sin_beta; # The same thing, going to use throughout
+        if cos_zenith[i-1] > 1.0:
+            cos_zenith[i-1] = 1.0
+        elif cos_zenith[i-1] < 0.0:
+            cos_zenith[i-1] = 0.0
+
+        zenith_angle = 180.0 / pi * (acos(cos_zenith[i-1]))
+        elevation[i-1] = 90.0 - zenith_angle
+
+    return (elevation, cos_zenith)
+
+
+def calculate_solar_noon(et, longitude):
+    """
+    Calculation solar noon - De Pury & Farquhar, '97: eqn A16
+    Reference:
+    ----------
+    * De Pury & Farquhar (1997) PCE, 20, 537-557.
+    Returns:
+    ---------
+    t0 - solar noon (hours).
+    """
+    # all international standard meridians are multiples of 15deg east/west of
+    # greenwich
+    Ls = round_to_value(longitude, 15.)
+    t0 = 12.0 + (4.0 * (Ls - longitude) - et) / 60.0
+
+    return (t0)
+
+def round_to_value(number, roundto):
+    return round(number / roundto) * roundto
+
+
+
+def calculate_solar_declination(doy, gamma):
+    """
+    Solar Declination Angle is a function of day of year and is indepenent
+    of location, varying between 23deg45' to -23deg45'
+    Arguments:
+    ----------
+    doy : int
+        day of year, 1=jan 1
+    gamma : double
+        fractional year (radians)
+    Returns:
+    --------
+    dec: float
+        Solar Declination Angle [radians]
+    Reference:
+    ----------
+    * De Pury & Farquhar (1997) PCE, 20, 537-557.
+    * Leuning et al (1995) Plant, Cell and Environment, 18, 1183-1200.
+    * J. W. Spencer (1971). Fourier series representation of the position of
+      the sun.
+    """
+
+    # declination (radians) */
+    #decl = 0.006918 - 0.399912 * cos(gamma) + 0.070257 * sin(gamma) - \
+    #       0.006758 * cos(2.0 * gamma) + 0.000907 * sin(2.0 * gamma) -\
+    #       0.002697 * cos(3.0 * gamma) + 0.00148 * sin(3.0 * gamma);*/
+
+    # (radians) A14 - De Pury & Farquhar
+    decl = -23.4 * (pi / 180.) * cos(2.0 * pi * (doy + 10) / 365)
+
+    return (decl)
+
+def calculate_hour_angle(t, t0):
+    """
+    Calculation solar noon - De Pury & Farquhar, '97: eqn A15
+    Reference:
+    ----------
+    * De Pury & Farquhar (1997) PCE, 20, 537-557.
+    Returns:
+    ---------
+    h - hour angle (radians).
+    """
+    return (pi * (t - t0) / 12.0)
+
+
+def day_angle(doy):
+    """
+    Calculation of day angle - De Pury & Farquhar, '97: eqn A18
+    Reference:
+    ----------
+    * De Pury & Farquhar (1997) PCE, 20, 537-557.
+    * J. W. Spencer (1971). Fourier series representation of the position of
+      the sun.
+    Returns:
+    ---------
+    gamma - day angle in radians.
+    """
+
+    return (2.0 * pi * (doy - 1.0) / 365.0)
+
+def calculate_eqn_of_time(gamma):
+    """
+    Equation of time - correction for the difference btw solar time
+    and the clock time.
+    Arguments:
+    ----------
+    doy : int
+        day of year
+    gamma : double
+        fractional year (radians)
+    References:
+    -----------
+    * De Pury & Farquhar (1997) PCE, 20, 537-557.
+    * Campbell, G. S. and Norman, J. M. (1998) Introduction to environmental
+      biophysics. Pg 169.
+    * J. W. Spencer (1971). Fourier series representation of the position of
+      the sun.
+    * Hughes, David W.; Yallop, B. D.; Hohenkerk, C. Y. (1989),
+      "The Equation of Time", Monthly Notices of the Royal Astronomical
+      Society 238: 1529-1535
+    """
+
+    # minutes - de Pury and Farquhar, 1997 - A17
+    et = (0.017 + 0.4281 * cos(gamma) - 7.351 * sin(gamma) - 3.349 *
+          cos(2.0 * gamma) - 9.731  * sin(gamma))
+
+    return (et)
+
+def calc_extra_terrestrial_irradiance(doy, cos_zenith):
+    """
+    Solar radiation incident outside the earth's atmosphere, e.g.
+    extra-terrestrial radiation. The value varies a little with the earths
+    orbit.
+    Using formula from Spitters not Leuning!
+    Arguments:
+    ----------
+    doy : double
+        day of year
+    cos_zenith : double
+        cosine of zenith angle (radians)
+    Returns:
+    --------
+    So : float
+        solar radiation normal to the sun's bean outside the Earth's atmosphere
+        (W m-2)
+    Reference:
+    ----------
+    * Spitters et al. (1986) AFM, 38, 217-229, equation 1.
+    """
+    Sc = 1370.0 # W m-2
+
+    # remember sin_beta = cos_zenith; trig funcs are cofuncs of each other
+    # sin(x) = cos(90-x) and cos(x) = sin(90-x).
+    So = Sc * (1.0 + 0.033 * cos(doy / 365.0 * 2.0 * pi)) * cos_zenith;
+
+    return (So)
+
+def estimate_clearness(sw_rad, So):
+    """
+    estimate atmospheric transmisivity - the amount of diffuse radiation
+    is a function of the amount of haze and/or clouds in the sky. Estimate
+    a proxy for this, i.e. the ratio between global solar radiation on a
+    horizontal surface at the ground and the extraterrestrial solar
+    radiation
+    """
+
+    # catch possible divide by zero when zenith = 90.
+    if So <= 0.0:
+        tau = 0.0
+    else:
+        tau = sw_rad / So
+
+    if tau > 1.0:
+        tau = 1.0;
+    elif tau < 0.0:
+        tau = 0.0
+
+    return (tau)
 
 if __name__ == "__main__":
 
